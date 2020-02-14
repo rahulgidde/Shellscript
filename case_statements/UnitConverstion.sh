984ef9910eadf6f106782890d018702f0ad78e1a
rahulgidde <rahulgidde1212@gmail.com>
2020-02-14 12:17:41 +0530

    MODIFIED:Variable Added

--- case_statements/UnitConverstion.sh
+++ case_statements/UnitConverstion.sh
@@ -4,33 +4,32 @@
 read -p "Enter the Number: " number
 
 #DISPLAYING THE AVALIABLE CHOICE
-echo "Enter Your Choice: "
 echo "1.Feet to Inch"
 echo "2.Inch to Feet"
 echo "3.Feet to Meter"
 echo "4.Meter to Feet"
 
 #GET CHOICE FROM USER
-read -p choice
+read -p "Enter Your Choice: " choice
 
 case $choice in
 	1)
-		echo "Convertion From Feet To Inch"
+		echo "Convertion From Feet To Inch:"
 		echo "scale=1;$number*12" | bc
 		;;
 	2)
-		echo "Convertion From Inch To Feet"
+		echo "Convertion From Inch To Feet:"
 		echo "scale=2;$number/12" | bc
 		;;
 	3)
-		echo "Convertion From Feet To Meter"
+		echo "Convertion From Feet To Meter:"
 		echo "scale=2;$number*0.3048" | bc
 		;;
 	4)
-		echo "Convertion From Meter To Feet"
+		echo "Convertion From Meter To Feet:"
 		echo "scal=2;$number/0.3048" | bc
 		;;
 	*)
-		echo "Invalid choice"
+		echo "Invalid choice:"
 		;;
 esac
